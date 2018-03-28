7ba2180d59d0760c7f5de5381bf25fd4945156be
R0ulito <roulit77@gmail.com>
2018-03-28 22:32:51 +0200

    Created all pages | Created album-item component ==> TODO : create artist-item component && fix the loader

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
 const searchUrl = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q="
 const trackUrl = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/"
+const albumUrl = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/"
 
 Vue.component('search-form', {
     data() {
         return {
             queryString: '',
+            queryOrder : '',
             searchUrl : searchUrl
 
         }
     },
     template: `<form @submit.prevent>
-        <div class="form-group"><input class="form-control" type="text" name="query" id="query" v-model.lazy="queryString" placeholder="Tapez le nom d'un artiste, d'un album, d'un titre"></div>
-        <div class="form-group"><select class="form-control" name="order" id="order">
-        <option value="">Trier la recherche par...</option>
-        <option value="album">Album</option>
-        <option value="artist">Artiste</option>
-        <option value="music">Musique</option>
-        <option value="trend">Popularité</option>
-        <option value="rating">Note</option>
-        </select></div>
-        <button class="btn btn-block btn-info" type="submit" @click="getAPIResult">Rechercher</button>
-    </form>`,
+                    <div class="form-group">
+                        <input class="form-control" type="text" name="query" id="query" v-model.lazy="queryString" placeholder="Tapez le nom d'un artiste, d'un album, d'un titre">
+                    </div>
+                    <div class="form-group">
+                        <select class="form-control" name="order" id="order" v-model="queryOrder">
+                            <option value="">Trier la recherche par...</option>
+                            <option value="&order​=ALBUM_ASC">Album</option>
+                            <option value="&​order​=ARTIST_ASC">Artiste</option>
+                            <option value="&​order​=TRACK_ASC">Musique</option>
+                            <option value="&​order​=RATING_ASC">Popularité</option>
+                            <option value="&order​=RANKING">Note</option>
+                        </select>
+                    </div>
+                    <button class="btn btn-block btn-info" type="submit" @click="getAPIResult">Rechercher</button>
+               </form>`,
     methods : {
         getAPIResult() {
-                const fullSearch = this.searchUrl + this.queryString;
+            console.log(this.queryOrder)
+            // document.querySelector('.loader').style.display = "block";
+                const fullSearch = this.searchUrl + this.queryString + this.queryOrder;
+            console.log(fullSearch)
                 fetch(fullSearch)
                     .then(data => data.json())
                     .then(data => {
@@ -35,7 +44,10 @@ Vue.component('search-form', {
             processResults() {
                 // console.log(this.$parent.queryResult);
             }
-        }
+        },
+    mounted() {
+        document.querySelector('.loader').style.display = "none";
+    }
     })
 
 Vue.component('navbar', {
@@ -49,28 +61,34 @@ Vue.component('navbar', {
 })
 
 Vue.component('track-item', {
-    template: ` <div v-if="track" class="col-xs-12">
-                    <div class="col-xs-12 row">
-                        <h2>Titre: {{track.title}}</h2>
-                    </div>
-                    <div class="col-xs-12 row">
-                        <div class="col-xs-6">
-                            <img :src="track.album.cover_small" alt=""> Album : {{track.album.title}}
+    template: `<div>
+                    
+                    <div v-if="track" class="col-xs-12">
+                        <div class="col-xs-12 row">
+                            <h2>Titre: {{track.title}}</h2>
                         </div>
-                        <div class="col-xs-6">
-                            <img :src="track.artist.picture_small" alt=""> Artiste : {{track.artist.name}}
+                        <div class="col-xs-12 row">
+                            <div class="col-xs-6">
+                                <img :src="track.album.cover_small" alt=""> Album : <a :href="'album.html?id=' + track.album.id">{{track.album.title}}</a>
+                            </div>
+                            <div class="col-xs-6">
+                                <img :src="track.artist.picture_small" alt=""> Artiste : <a :href="'artist.html?id=' + track.artist.id">{{track.artist.name}}</a>
+                            </div>
                         </div>
+                        <div class="row col-xs-12">
+                            <h2>{{track.title}}</h2>
+                            <div>Durée: {{track.duration | secToMin}} / Date de parution: {{track.release_date | dateFilter }}</div>
+                        </div>
+                        <div class="row col-xs-12">
+                            <audio controls>
+                                <source :src="track.preview" type="audio/mpeg">                
+                            </audio>
+                        </div>        
                     </div>
-                    <div class="row col-xs-12">
-                        <h2>{{track.title}}</h2>
-                        <div>Durée: {{track.duration | secToMin}} / Date de parution: {{track.release_date | dateFilter }}</div>
+                    <div v-else>
+                        <div class="loader"></div>
                     </div>
-                    <div class="row col-xs-12">
-                        <audio controls>
-                            <source :src="track.preview" type="audio/mpeg">                
-                        </audio>
-                    </div>        
-                </div>
+               </div>
     `,
     data () {
         return {
@@ -101,7 +119,38 @@ Vue.component('track-item', {
                 this.track = data;
                 
                 console.log(this.track)
-                // this.processResults();
+
+            });
+        // document.querySelector('.loader').classList.add('hidden');
+
+    }
+});
+
+Vue.component('album-item', {
+    template : `<div>
+                    <div v-if="album">
+                    {{album}}
+                    </div>
+                    <div v-else>
+                        <div class="loader"></div>
+                    </div>                    
+                </div>`,
+    data() {
+        return {
+            id : document.location.search.split('=')[1],
+            album : ''
+        }
+    },
+    mounted() {
+        const fullSearch = albumUrl + this.id;
+        console.log(fullSearch)
+        fetch(fullSearch)
+            .then(data => data.json())
+            .then(data => {
+                this.album = data;
+
+                console.log(this.track)
+
             });
 
     }
